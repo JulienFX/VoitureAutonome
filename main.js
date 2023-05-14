7cf112aaafc009f64df70d66f252c447fa05f119
JulienFX <jujustor@gmail.com>
2023-05-14 19:37:20 +0100

    changement de la visu : affichage d'un fond noir pour réseau neuronne

--- main.js
+++ main.js
@@ -1,10 +1,13 @@
-const canvas=document.getElementById("myCanvas")
-canvas.width=200 // en général on fait modification dans js quand l'objet va être en intéraction avec l'utilisateur
+const voitureCanvas=document.getElementById("voitureCanvas")
+voitureCanvas.width=200 // en général on fait modification dans js quand l'objet va être en intéraction avec l'utilisateur
+const reseauCanvas=document.getElementById("reseauCanvas")
+reseauCanvas.width=300 
 
-const ctx = canvas.getContext("2d") // le contexte permet de savoir comment sera le rendu des dessins : 2D/3D 
+const voitureCtx = voitureCanvas.getContext("2d") // le contexte permet de savoir comment sera le rendu des dessins : 2D/3D 
+const reseauCtx = reseauCanvas.getContext("2d") 
 let nbVoies=0
 do{ nbVoies = getNbIntAleat(6)}while(nbVoies<=1)
-const route = new Route(canvas.width/2,canvas.width*0.9,nbVoies) // avant => 2 params
+const route = new Route(voitureCanvas.width/2,voitureCanvas.width*0.9,nbVoies) // avant => 2 params
 const voiture = new Voiture(route.getCentreVoie(getNbIntAleat(6-1)),100,30,50,"IA") // choix de la voie avec le x de la voiture 
 // avant => route.getCentreVoie(2)
 const traffic =[new Voiture(route.getCentreVoie(getNbIntAleat(6-1)),-100,30,50,"AUTRES",2)]
@@ -15,13 +18,14 @@ function animer(){
         traffic[i].maj(route.bords,[])
     }
     voiture.maj(route.bords,traffic)
-    canvas.height= window.innerHeight // refresh coordonnées voiture 
-    ctx.save()
-    ctx.translate(0,-voiture.y+canvas.height*0.7)
-    route.draw(ctx)
+    voitureCanvas.height= window.innerHeight // match taille avec écran
+    reseauCanvas.height= window.innerHeight
+    voitureCtx.save()
+    voitureCtx.translate(0,-voiture.y+voitureCanvas.height*0.7)
+    route.draw(voitureCtx)
     for(let i=0;i<traffic.length;i++){
-        traffic[i].draw(ctx)
+        traffic[i].draw(voitureCtx)
     }
-    voiture.draw(ctx)
+    voiture.draw(voitureCtx)
     requestAnimationFrame(animer)
 }
