3f590794f6dd57bd8a4b9cc09783a5bf17d650e1
JulienFX <jujustor@gmail.com>
2023-05-16 15:52:36 +0100

    ajout d'autres voitures

--- main.js
+++ main.js
@@ -1,31 +1,42 @@
 const voitureCanvas=document.getElementById("voitureCanvas")
 voitureCanvas.width=200 // en général on fait modification dans js quand l'objet va être en intéraction avec l'utilisateur
-const reseauCanvas=document.getElementById("reseauCanvas")
-reseauCanvas.width=300 
+
 
 const voitureCtx = voitureCanvas.getContext("2d") // le contexte permet de savoir comment sera le rendu des dessins : 2D/3D 
-const reseauCtx = reseauCanvas.getContext("2d") 
 let nbVoies=0
 do{ nbVoies = getNbIntAleat(6)}while(nbVoies<=1)
 const route = new Route(voitureCanvas.width/2,voitureCanvas.width*0.9,nbVoies) // avant => 2 params
-const voiture = new Voiture(route.getCentreVoie(getNbIntAleat(6-1)),100,30,50,"IA") // choix de la voie avec le x de la voiture 
+const voitures = genererVoiture(100)//new Voiture(route.getCentreVoie(getNbIntAleat(6-1)),100,30,50,"IA") // choix de la voie avec le x de la voiture 
 // avant => route.getCentreVoie(2)
 const traffic =[new Voiture(route.getCentreVoie(getNbIntAleat(6-1)),-100,30,50,"AUTRES",2)]
 
 animer()
+
+function genererVoiture(N){
+    const voitures=[]
+    for (let i=1;i<=N;i++){
+        voitures.push(new Voiture(route.getCentreVoie(getNbIntAleat(6-1)),100,30,50,"IA"))
+    }
+    return voitures
+}
 function animer(){
     for(let i=0;i<traffic.length;i++){
         traffic[i].maj(route.bords,[])
     }
-    voiture.maj(route.bords,traffic)
+    for (let i=0;i<voitures.length;i++){
+        voitures[i].maj(route.bords,traffic)
+    }
     voitureCanvas.height= window.innerHeight // match taille avec écran
-    reseauCanvas.height= window.innerHeight
     voitureCtx.save()
-    voitureCtx.translate(0,-voiture.y+voitureCanvas.height*0.7)
+    voitureCtx.translate(0,-voitures[0].y+voitureCanvas.height*0.7)
     route.draw(voitureCtx)
     for(let i=0;i<traffic.length;i++){
         traffic[i].draw(voitureCtx)
     }
-    voiture.draw(voitureCtx)
+    for(let i=0;i<voitures.length;i++){
+        voitures[i].draw(voitureCtx)
+    }
+    
+    voitureCtx.restore()
     requestAnimationFrame(animer)
 }
