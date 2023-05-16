5f11e6a4669ac3b5f7f07e5c3938fd869cac7ac2
JulienFX <jujustor@gmail.com>
2023-05-16 16:47:30 +0100

    boutton de sauvegarde

--- main.js
+++ main.js
@@ -4,13 +4,25 @@ voitureCanvas.width=200 // en général on fait modification dans js quand l'obj
 
 const voitureCtx = voitureCanvas.getContext("2d") // le contexte permet de savoir comment sera le rendu des dessins : 2D/3D 
 let nbVoies=0
+
 do{ nbVoies = getNbIntAleat(6)}while(nbVoies<=1)
 const route = new Route(voitureCanvas.width/2,voitureCanvas.width*0.9,nbVoies) // avant => 2 params
 const voitures = genererVoiture(100)//new Voiture(route.getCentreVoie(getNbIntAleat(6-1)),100,30,50,"IA") // choix de la voie avec le x de la voiture 
 // avant => route.getCentreVoie(2)
 const traffic =[new Voiture(route.getCentreVoie(getNbIntAleat(6-1)),-100,30,50,"AUTRES",2)]
-
+let meilleurVoiture=voitures[0]
+if(localStorage.getItem("meilleurCerveau")){
+    meilleurVoiture.cerveau=JSON.parse(localStorage.getItem("meilleurCerveau"))
+}
 animer()
+
+function sauvegarde(){
+    localStorage.setItem("meilleurCerveau",JSON.stringify(meilleurVoiture.cerveau))
+}
+
+function supprimer(){
+    localStorage.removeItem("meilleurCerveau")
+}
 
 function genererVoiture(N){
     const voitures=[]
@@ -27,7 +39,7 @@ function animer(){
     for (let i=0;i<voitures.length;i++){
         voitures[i].maj(route.bords,traffic)
     }
-    const meilleurVoiture = voitures.find(c=>c.y==Math.min(... voitures.map(c=>c.y))) // ... => permet de spread le tableau 
+    meilleurVoiture = voitures.find(c=>c.y==Math.min(... voitures.map(c=>c.y))) // ... => permet de spread le tableau 
     voitureCanvas.height= window.innerHeight // match taille avec écran
     voitureCtx.save()
     voitureCtx.translate(0,-meilleurVoiture.y+voitureCanvas.height*0.7)
